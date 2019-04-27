d78fe82a50f86293c1090b299e7a8891bc2d7060
Matt Kern <mjkern@andrew.cmu.edu>
2019-04-27 19:52:17 -0400

    popular items partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
     end
     addresses.map{|addr| ["#{addr.recipient} : #{addr.street_1}", addr.id] }
   end
+
+  def get_pop_items(count=4)
+    Item.all.joins(:order_items).group("id").order("SUM(quantity)").limit(count)
+  end
 end
